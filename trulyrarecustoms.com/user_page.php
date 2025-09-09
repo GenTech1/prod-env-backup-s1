3c6610d7caaa9eb21d520da5bfb6e084b1833265
Nathan141-Red <natewashington9765@gmail.com>
2025-09-09 13:33:12 -0500

    Font update 1.3

--- trulyrarecustoms.com/user_page.php
+++ trulyrarecustoms.com/user_page.php
@@ -56,6 +56,12 @@ exit;
     <div class="blackback">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="user_page.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div id="homeLogo">
     <img id="logo1" src="assets/logo1.png" alt="Logo1"/>
