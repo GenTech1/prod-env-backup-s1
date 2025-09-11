3ef3ec647b25b06925ed8e204950bddf0e3d46e1
Quintont-zaxonline <quintont@zax-online.com>
2025-09-11 11:14:19 -0500

    allows admin user to change DB

--- trulyrarecustoms.com/adminChanges.php
+++ trulyrarecustoms.com/adminChanges.php
@@ -20,8 +20,16 @@ try {
 	for($i=0;$i<count($products);$i++){
 	$id = $_POST['id'];
 	if($products[$i]['id'] == $id){
-	$stmt = $pdo ->query("UPDATE Products SET name =" . POST['name'] . "WHERE id =1");
-}
+	$stmt = $pdo ->query("UPDATE Products SET name ='" . $_POST['name'] . "' WHERE id ='" . $_POST['id'] . "'");
+    $stmt = $pdo ->query("UPDATE Products SET description ='" . $_POST['description'] . "' WHERE id ='" . $_POST['id'] . "'");
+    $stmt = $pdo ->query("UPDATE Products SET image ='" . $_POST['image'] . "' WHERE id ='" . $_POST['id'] . "'");
+    $stmt = $pdo ->query("UPDATE Products SET price ='" . $_POST['price'] . "' WHERE id ='" . $_POST['id'] . "'");
+    $stmt = $pdo ->query("UPDATE Products SET currency ='" . $_POST['currency'] . "' WHERE id ='" . $_POST['id'] . "'");
+    $stmt = $pdo ->query("UPDATE Products SET image ='" . str_replace('\\', '\\\\', $_POST['image']) . "' WHERE id ='" . $_POST['id'] . "'");
+    $stmt = $pdo ->query("UPDATE Products SET tags ='" . $_POST['tags'] . "' WHERE id ='" . $_POST['id'] . "'");
+    $stmt = $pdo ->query("UPDATE Products SET stock ='" . $_POST['stock'] . "' WHERE id ='" . $_POST['id'] . "'");
+        $stmt = $pdo ->query("UPDATE Products SET `visible/not visible` ='" . $_POST['visible'] . "' WHERE id ='" . $_POST['id'] . "'");
+}   
 }
 
 } catch (PDOException $e) {
