995a6364ca24abc7d71f0ffbeff8520735adf1f7
Nathan141-Red <natewashington9765@gmail.com>
2025-10-13 11:34:14 -0700

    Email 0.1

--- trulyrarecustoms.com/subscribe_email.php
+++ trulyrarecustoms.com/subscribe_email.php
@@ -8,33 +8,113 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 $api_key = getenv('WAFFLE');
 $list_id = getenv('WAFFLE_KINGDOM');
 
-$payload = [
-	"profiles" => [
-		['email' => $email]
+try{
+	$payload = [
+		"data" => [
+			"type" => "profile",
+			"attributes" => [
+			"email" => $email
+		]
 	]
 ];
 
-// $endpoint = "https://a.klaviyo.com/api/v2/list/{$list_id}/subscribe?api_key={$api_key}";
 
-$ch = curl_init($endpoint);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://a.klaviyo.com/api/profiles");
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
-	"Content-Type: application/json"
+  'Authorization: Klaviyo-API-Key ' . $api_key,
+  'Revision: 2023-10-01',
+   'Accept: application/vnd.api+json',
+  "Content-Type: application/json"
 ]);
-
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
-
-echo $response
-
-if ($httpCode === 200) {
-	echo "HI World";
-} else {
-	echo "It didn't work go back!!!";
+echo ($response);
+}catch(Exception $e){
+    http_response_code(500);
+    echo json_encode(['error' => 'Internal Server Error']);
+    exit;
 }
-?>
 
+
+
+
+if ($code >= 200 && $code < 300) {
+	$profileId = json_decode($response, true)['data'][0]['id'];
+	$payload = [
+		"data" => [
+			[
+				"type" => "profile",
+				"id" => $profileId
+			]
+		]
+	];
+	try{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://a.klaviyo.com/api/lists/$list_id/relationships/profiles");
+		curl_setopt($ch, CURLOPT_HTTPHEADER, [
+  		'Authorization: Klaviyo-API-Key ' . $api_key,
+  		'Revision: 2023-10-01',
+   		'Accept: application/vnd.api+json',
+  		"Content-Type: application/json"
+]);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	if($response == NULL){
+		echo ' cURL error: ' . curl_error($ch);
+	} else {
+		echo 'profile added to list';
+		$response = curl_exec($ch);
+		echo $response;
+	}
+}catch(Exception $e){
+    http_response_code(500);
+    echo json_encode(['error' => 'Internal Server Error']);
+    exit;
+}
+}  else {
+	http_response_code($code);
+	echo $response;
+}
+
+// try {
+// 	$payload = [
+// 		"data" => [
+// 			"type" => "profile",
+// 			"attributes" => [
+// 			'email' => $email
+// 		]
+// 	]
+// ];
+// }
+echo $response;
+
+// move it to Master
+
+try {
+	$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "'https://a.klaviyo.com/api/profiles?filter=equals(email,'$email')'");
+		curl_setopt($ch, CURLOPT_HTTPHEADER, [
+  		'Authorization: Klaviyo-API-Key ' . $api_key,
+  		'Revision: 2023-10-01',
+   		'Accept: application/vnd.api+json',
+]);
+curl_setopt($ch, CURLOPT_POST, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+}catch(Exception $e){
+    http_response_code(500);
+    echo json_encode(['error' => 'Internal Server Error']);
+    exit;
+}
+echo $response;
