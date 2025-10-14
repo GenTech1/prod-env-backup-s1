e8be83c03b19a4269df0a0062384090992ece714
Nathan141-Red <natewashington9765@gmail.com>
2025-10-14 09:37:34 -0700

    Email 0.2

--- trulyrarecustoms.com/subscribe_email.php
+++ trulyrarecustoms.com/subscribe_email.php
@@ -42,8 +42,27 @@ echo ($response);
     exit;
 }
 
+// move it to Master
 
+try {
+	$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://a.klaviyo.com/api/profiles?filter=equals(email,'$email')");
+		curl_setopt($ch, CURLOPT_HTTPHEADER, [
+  		'Authorization: Klaviyo-API-Key ' . $api_key,
+  		'Revision: 2023-10-01',
+   		'Accept: application/vnd.api+json',
+]);
+curl_setopt($ch, CURLOPT_POST, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+$response = curl_exec($ch);
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+}catch(Exception $e){
+    http_response_code(500);
+    echo json_encode(['error' => 'Internal Server Error']);
+    exit;
+}
 
 if ($code >= 200 && $code < 300) {
 	$profileId = json_decode($response, true)['data'][0]['id'];
@@ -84,37 +103,62 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	echo $response;
 }
 
-// try {
-// 	$payload = [
-// 		"data" => [
-// 			"type" => "profile",
-// 			"attributes" => [
-// 			'email' => $email
-// 		]
-// 	]
-// ];
-// }
 echo $response;
 
-// move it to Master
-
 try {
-	$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "'https://a.klaviyo.com/api/profiles?filter=equals(email,'$email')'");
-		curl_setopt($ch, CURLOPT_HTTPHEADER, [
-  		'Authorization: Klaviyo-API-Key ' . $api_key,
-  		'Revision: 2023-10-01',
-   		'Accept: application/vnd.api+json',
+	$payload = [
+	"data"=> [
+		"type"=> "profile-subscription-bulk-create-job",
+		"attributes" => [
+			"profiles"=> [
+				"data"=> [
+					[
+						"type"=> "profile",
+						"attributes"=> [
+							"email"=> "$email",
+							"subscriptions"=> [
+								"email"=> [
+									"consent"=> "SUBSCRIBE"
+								],
+							]
+						],
+						"id"=> "$profileId"
+					]
+				]
+					],
+					"historical_import"=> false
+				],
+				"relationships"=> [
+					"list"=> [
+						"data"=> [
+							"type"=> "list",
+							"id"=> "$list_id"
+						]
+					]
+				]
+	]
+		];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://a.klaviyo.com/api/profile-subscription-bulk-create-jobs");
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+  'Authorization: Klaviyo-API-Key ' . $api_key,
+  'Revision: 2025-07-15',
+   'Accept: application/vnd.api+json',
+  "Content-Type: application/json"
 ]);
-curl_setopt($ch, CURLOPT_POST, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
 $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+echo $response;
 curl_close($ch);
-}catch(Exception $e){
+} catch(Exception $e){
     http_response_code(500);
     echo json_encode(['error' => 'Internal Server Error']);
     exit;
 }
-echo $response;
+
+?>
