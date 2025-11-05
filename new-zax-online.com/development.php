b2475b51e44873b28445329e399c5826c6adf534
Nathan141-Red <natewashington9765@gmail.com>
2025-11-05 12:05:38 -0800

    development 0.4

--- new-zax-online.com/development.php
+++ new-zax-online.com/development.php
@@ -3,39 +3,76 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" rel="stylesheet" href="css/development.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="css/development.css">
     <title>Zax|Development</title>
 </head>
 <body> 
-    <nav>
-        <ul class="navbar">
-            <li><a href="index.php">Home Page</a></li>
-            <li><a href="development.php">Development</a></li>
-            <li><a href="#">Gaming</a></li>
-            <li><a href="#">Workstations</a></li>
-        </ul>
-    </nav>
-    
-    <div class="content">
-        <h1 id="reinvent">Reinvent</h1>
-        <h1 id="wheelz">Wheelz</h1>
-        <h2 id="develop">Develop And Defend</h2>
-        <p>
-            Protecting your online presence is crucial. We help clients develop and protect crucial systems
-        </p>
-    </div>
+    <section class="top-section">
+        <video autoplay muted loop id="bg-video">
+            <source src="../images/spaceEarth.mp4" type="video/mp4">
+        </video>
 
-    <div class="card text-bg-dark">
-        <img src="../images/computerSet.jpg" class="card-img">
-        <div class="card-img-overlay">
-            <h5 class="card-title">Research Report </h5>
-            <p class="card-text">Hi world</p>
-            <p class="card-text">The peanuts crew was here</p>
+        <div class="overlay-content">
+            <nav>
+                <ul class="navbar">
+                    <li><a href="index.php">Home Page</a></li>
+                    <li><a href="development.php">Development</a></li>
+                    <li><a href="#">Gaming</a></li>
+                    <li><a href="#">Workstations</a></li>
+                </ul>
+            </nav>
+    
+            <div class="content"> <!-- content is hero-text -->
+                <div class="content-left">
+                    <h1 class="page-title">Reinvent Wheelz</h1>
+                </div>
+                <div class="cobtent-right">
+                    <h4 class="page-subtitle">Develop And Defend</h4>
+                    <p class="page-description">
+                     Protecting your online presence is crucial. We help clients develop and protect crucial systems
+                    </p>
+                </div>
+            </div>
+        </div>    
+    </section>
+
+    <section class="bottom-section">
+        <div class="card-container d-flex justify-content-center gap-4 flex-wrap">
+            <div class="card text-bg-dark custom-card">
+                <img src="../images/computerSet.jpg" class="card-img" alt="Computer set">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Research Report </h5>
+                            <div class="card-hover-text">
+                                <p class="card-text">Hi world</p>
+                                <p class="card-text">The peanuts crew was here</p>
+                            </div>
+                    </div>
+            </div>
+
+            <div class="card text-bg-dark custom-card">
+                <img src="../images/computerSet.jpg" class="card-img" alt="Computer set">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Research Report </h5>
+                            <div class="card-hover-text">
+                                <p class="card-text">Hi world</p>
+                                <p class="card-text">The peanuts crew was here</p>
+                            </div>
+                    </div>
+            </div>
+
+            <div class="card text-bg-dark custom-card">
+                <img src="../images/computerSet.jpg" class="card-img" alt="Computer set">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Research Report </h5>
+                            <div class="card-hover-text">
+                                <p class="card-text">Hi world</p>
+                                <p class="card-text">The peanuts crew was here</p>
+                            </div>
+                    </div>
+            </div>
         </div>
-    </div>
-    <video autoplay muted loop id="bg-video">
-        <source src="../images/spaceEarth.mp4" type="video/mp4">
-    </video>
+    </section>
 </body>
 </html>
