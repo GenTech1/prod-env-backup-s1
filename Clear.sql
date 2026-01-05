6de209f6357bbff45d37006e072c03060b051c99
quintont-zaxonline <quintont@zax-online.com>
2026-01-05 13:01:08 -0600

    clear.sql pt 2

--- Clear.sql
+++ Clear.sql
@@ -1,11 +1,1 @@
--- Generate DROP DATABASE statements for all databases
--- except system / protected ones
-
-SELECT CONCAT('DROP DATABASE `', schema_name, '`;') AS drop_statement
-FROM information_schema.schemata
-WHERE schema_name NOT IN (
-    'information_schema',
-    'mysql',
-    'performance_schema',
-    'phpmyadmin'
-);
+Drop DATABASE IF EXISTS `domus_users`;
